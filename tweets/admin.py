01626a3284f98948e0dce4a9ebddd0ff44249374
Engineer88 <yjh2901@naver.com>
2024-09-20 17:40:00 +0900

    chg_airbnb_day5
    chg_airbnb_day5

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Tweet, Like
 
-# Register your models here.
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "payload",
+        "user",
+    )
+
+    search_fields = (
+        "payload",
+        "user__username",
+    )
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+
+    search_fields = ("user__username",)
+
+    list_filter = ("created_at",)
--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class Tweet(CommonModel):
+    """Tweet Model Definition"""
+
+    payload = models.CharField(max_length=180)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="tweets",
+    )
+
+    def __str__(self):
+        return self.payload
+
+
+class Like(CommonModel):
+    """Like Model Definition"""
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
+    tweet = models.ForeignKey(
+        "tweets.Tweet",
+        on_delete=models.CASCADE,
+        related_name="likes",
+    )
+
+    def __str__(self):
+        return f"{self.tweet.payload}"
