e5ec4162925482976cebebbfd33fc95754ecc5b8
Engineer88 <yjh2901@naver.com>
2024-09-24 23:45:59 +0900

    chg_airbnb_day9
    chg_airbnb_day9

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
+class ElonFilter(admin.SimpleListFilter):
+
+    title = "Filter by like count"
+
+    parameter_name = "likes"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("elon", "Tweets with Elon"),
+            ("no_elon", "Tweets without Elon"),
+        ]
+
+    def queryset(self, request, tweets):
+        likes = self.value()
+        if likes == "elon":
+            return tweets.filter(payload__contains="Elon Musk")
+        elif likes == "no_elon":
+            return tweets.exclude(payload__contains="Elon Musk")
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
 
@@ -14,6 +34,11 @@ class TweetAdmin(admin.ModelAdmin):
     search_fields = (
         "payload",
         "user__username",
+    )
+
+    list_filter = (
+        ElonFilter,
+        "created_at",
     )
 
 
