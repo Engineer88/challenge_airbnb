ba6607e98e00f684de3be55056d40c474496a7cd
Engineer88 <yjh2901@naver.com>
2024-09-27 17:00:41 +0900

    chg_airbnb_day12
    chg_airbnb_day12

--- tweets/serializer.py
+++ tweets/serializer.py
@@ -1,12 +1,8 @@
-from rest_framework import serializers
-from users.models import User
+from rest_framework.serializers import ModelSerializer
+from tweets.models import Tweet
 
 
-class TweetSerializer(serializers.Serializer):
-
-    pk = serializers.IntegerField()
-    payload = serializers.CharField(
-        required=True,
-    )
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    created_at = serializers.DateTimeField()
+class TweetSerializer(ModelSerializer):
+    class Meta:
+        model = Tweet
+        fields = "__all__"
--- tweets/views.py
+++ tweets/views.py
@@ -1,40 +1,11 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import TweetSerializer
 from .models import Tweet
-from tweets.serializer import TweetSerializer
-from users.models import User
-from rest_framework import status
-from rest_framework.exceptions import NotFound
 
 
-@api_view()
-def tweets(request):
-    all_tweets = Tweet.objects.all()
-    seriallizer = TweetSerializer(
-        all_tweets,
-        many=True,
-    )
-
-    return Response(
-        {
-            "tweets": seriallizer.data,
-        }
-    )
-
-
-@api_view()
-def user_tweets(request, user_pk):
-    try:
-        user_id = User.objects.get(pk=user_pk)
-    except User.DoesNotExist:
-        raise NotFound(detail="User not found", code=status.HTTP_404_NOT_FOUND)
-    user_tweets = Tweet.objects.filter(user=user_id)
-    seriallizer = TweetSerializer(
-        user_tweets,
-        many=True,
-    )
-    return Response(
-        {
-            "user_tweets": seriallizer.data,
-        }
-    )
+class AllTweets(APIView):
+    def get(self, request):
+        tweets = Tweet.objects.all()
+        serializer = TweetSerializer(tweets, many=True)
+        return Response(serializer.data)
